e708129bea38f99d53dc313ed7d0159f42813915
Ognjen Jovanovic <jovanovicogi@yandex.com>
2021-02-08 11:17:33 +0100

    sample

--- src/App.js
+++ src/App.js
@@ -35,13 +35,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
+        <h1>Example heading <span className="badge bg-secondary">New</span></h1>
+        <h2>Example heading <span className="badge bg-secondary">New</span></h2>
       </div>
     )
   }
